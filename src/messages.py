49f4056a6408b6204697fe9215b7bd099a76b794
Seb <75587701+BobEsponjeYT@users.noreply.github.com>
2021-08-03 00:11:16 -0300

    Update messages.py

--- src/messages.py
+++ src/messages.py
@@ -1,9 +1,15 @@
 class messages:
   
+  ## ON PARTY
+  
   on_lose = "Perdistes!"
   
   on_tie = "Empate!"
   
   on_win = "Ganastes!"
   
+  ## ON MENU
+  
   no_option = "No elegistes una opcion!"
+  
+  no_nickname = "No pusistes un nickname!"
