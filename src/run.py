17eebc32cdab71f5f3c5c6c5cca69d2da99a5905
Seb <75587701+iSebDev@users.noreply.github.com>
2021-10-17 22:03:11 -0300

    Updated
    Repaired problems & added colors

--- src/run.py
+++ src/run.py
@@ -1,6 +1,6 @@
-import json
 import random
 from messages import messages
+from os import name, system
 
 datos = {
     "piedra": "1",
@@ -9,6 +9,14 @@ datos = {
 }
 
 def login():
+    if name == "nt":
+        _ = system('cls')
+    elif "nt" in name:
+        _ = system('cls')
+
+    else:
+        _ = system('clear')
+
     
     user = input(f"{messages.nick_set}: ") ## Nicknames no funcionan bien despues del reinicio!!
     
@@ -32,12 +40,12 @@ def login():
 class end:
     def lose(u=None):
     
-        print(f"{messages.on_lose}")
+        print(f"\u001b[44m\u001b[31;1m{messages.on_lose}\u001b[0m")
     
         return start(u=u)
     def win(u=None):
         
-        print(f"{messages.on_win}")
+        print(f"\u001b[44m\u001b[32;1m{messages.on_win}\u001b[0m")
         
         return start(u=u)
 
@@ -65,7 +73,7 @@ def game(con=None, nick=None):
     
     if(con == bot_selection):
         
-        print(f"{messages.on_tie}")
+        print(f"\u001b[36m{messages.on_tie}")
         return start()
 
     else:
@@ -121,38 +129,38 @@ def game(con=None, nick=None):
 def start(u=None):
     while True:
     
-        print("_________________________________________________")
-
-        print(f"{messages.stone} (1), {messages.paper} (2) o {messages.scissors} (3) !")
-
-        pptp = input("- ")
-
+        print("\u001b[37;1m\u001b[1m_________________________________________________")
+        
+        print(f"\u001b[36;1m{messages.stone} (1), {messages.paper} (2) o {messages.scissors} (3) !")
+        
+        pptp = input("\u001b[33m- \u001b[36m")
+        
         if not pptp:
-
-            print(f"{messages.no_option}")
-
+            
+            print(f"\u001b[31m{messages.no_option}")
+            
         else:
-
+            
             if datos["piedra"] in pptp:
-
-                print("{}(Yo/Me): {}".format(u, messages.stone))
-
+                
+                print("\u001b[32m{}(Yo/Me): \u001b[37m{}".format(u, messages.stone))
+                
                 game(con=datos["piedra"], nick=u)
-
+                
             elif datos["papel"] in pptp:
-
-                print("{}(Yo): {}".format(u, messages.paper))
-
+                
+                print("\u001b[32m{}(Yo/Me): \u001b[37m{}".format(u, messages.paper))
+                
                 game(con=datos["papel"], nick=u)
-
+                
             elif datos["tijera"] in pptp:
-
-                print("{}(Yo): {}".format(u, messages.scissors))
-
+                
+                print("\u001b[32m{}(Yo/Me): \u001b[37m{}".format(u, messages.scissors))
+                
                 game(con=datos["tijera"], nick=u)
-
+                
             else:
-
+                
                 print(f"{messages.option_null}")
                 return start(u=u)
     
